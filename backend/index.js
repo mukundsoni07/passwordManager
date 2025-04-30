f4b114969c96056137b672da4a523ddb5abc7148
Mukund Soni <59203417+mukundsoni07@users.noreply.github.com>
2025-04-30 13:55:04 +0530

    Updated URL

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://password-manager-omega-one.vercel.app/", credentials: true }));
 
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
